473b120d37fcad7624924ae7aeecf567ba6d069a
Arian Chenari <chenariarian2@gmail.com>
2025-07-26 09:11:14 -0700

    lesson3 (function components)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-  const message = 'Hello World!';//adding javascript variables to html
 
   return (
-    <div className="App">
-      <h1>{message}</h1>{/*has a explantion for jsx*/}
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p>some text</p>{/*simple html value*/}
-      <p>{'some text'}</p>{/*text and string can be value*/}
-      <p>{12}</p>{/*intigers are valif value*/}
-      {/*<p>{{name: 'Arian', id: 1}}</p> objects are not valid in jsx*/}
+    <div className='App'>
+      <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,16 @@
+//Contet component of JSX
+const Content = () => {
+    const pickRandomName = () => {
+        const name = ['arian', 'arash', 'koroush'];
+        const index = Math.floor(Math.random() * 3);
+        return name[index];
+    }
+
+    return(
+        <main>
+            <p>Hello {pickRandomName()}!</p>{/*using () cause a run early when page run*/}
+        </main>
+    )
+}
+
+export default Content
