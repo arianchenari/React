5656e9d19cf71c49101c26822c0f949818ba26a7
Arian Chenari <chenariarian2@gmail.com>
2025-07-27 03:14:11 -0700

    lesson6 (useState Hook)

--- src/Content.js
+++ src/Content.js
@@ -1,36 +1,31 @@
+import { useState } from "react";
+
 //Contet component of react
 const Content = () => {
+    //useState(initialValue) return a array witch have 2 value : [state, setState()]
+    const [name, setName] = useState('arian')//set name as def arian and have setName func to change it
+    const [count, setCount] = useState(0)
+
     const pickRandomName = () => {
         const name = ['arian', 'arash', 'koroush'];
         const index = Math.floor(Math.random() * 3);
-        return name[index];
+        setName(name[index]);
     }
 
-    const pickRandomName2 = () => {
-        const name = ['arian', 'arash', 'koroush'];
-        const index = Math.floor(Math.random() * 3);
-        console.log(name[index]);
-    }
-    
-    const pickRandomName3 = (name) => {
-        console.log(name);
-    }
-
-    const pickRandomName4 = (e) => {
-        console.log(e)
-        console.log(e.target)
-        console.log(e.target.innerText)
+    const changeNumber = () => {
+        setCount(count + 1);//expected 1 and it is 0
+        //when we click button it actully show the state of count witch is 0 and then plus it to one so it 
+        // is 1 in next click then we change that to another state and in the next click it will show up 2
+        setCount(count + 1);//and it is in the first 0 + 1 , and also after code 16 becuase we talk about 
+        // state not a value and state of count in the first is zero also
+        console.log(count);//actully show the state not a final value
     }
 
     return(
         <main>
-            <p onDoubleClick={pickRandomName2}>{/*using another click event */}
-                Double Click!
-            </p>
-            <p>Hello {pickRandomName()}!</p>{/*using () cause a run early when page run*/}
-            <button onClick={pickRandomName2}>Click it</button>{/*don`t use  () cause a when clicking we want run */}
-            <button onClick={() => pickRandomName3('Arian')}>Click it</button>{/*when we want to use argumant for click event we use anounymus func for enter the values */}
-            <button onClick={(e) => pickRandomName4(e)}>Click it</button>{/*in a anounymus func we have 1 parammeter it says a event object*/}
+            <p onDoubleClick={pickRandomName}>Hello {name}!</p>{/*using () cause a run early when page run*/}
+            <button onClick={pickRandomName}>Change name</button>
+            <button onClick={changeNumber}>Count</button>
         </main>
     )
 }
