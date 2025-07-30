f141240e44ebe5c789b843dbff34efd9e01f7239
Arian Chenari <chenariarian2@gmail.com>
2025-07-30 03:09:43 -0700

    lesson11 (useEffect Hook)

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import AddItem from './AddItem';
 import SearchItem from './SearchItem';
 import Content from './Content';
 import Footer from './Footer';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('listItems')));
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('listItems')) || []);
   const [newItem, setNewItem] = useState('');
   const [search, setSearch] = useState('');
 
-  function setAndSaveItems(listItems) {
-    setItems(listItems);
-    localStorage.setItem('listItems', JSON.stringify(listItems));
-  }
+    useEffect(() => {//asyncourons code
+      localStorage.setItem('listItems', JSON.stringify(items));
+    }, [items]);///the second parameter using when running useEfefect if is null it run every change component and if is [] then run the useeffect one in begging
 
   function addItem(item) {
     const id = items.length ? items[items.length - 1].id + 1 : 1
     const myNewItem = {id, checked: false, item}
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems)
+    setItems(listItems);
   }
 
   function handleSubmit(e) {
@@ -30,12 +29,12 @@ function App() {
 
   function handleCheck(id) {
     const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item)
-    setAndSaveItems(listItems)
+    setItems(listItems);
   }
 
   function handleDelete(id) {
     const listItems = items.filter((item) => item.id !== id ? item : '')
-    setAndSaveItems(listItems)
+    setItems(listItems);
   }
 
   return (
